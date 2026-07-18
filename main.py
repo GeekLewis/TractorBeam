6e98bf97159315fc2c66323b7e2c7b2ac70b8717
Lewis Miller <lewis.miller@gmail.com>
2026-07-17 23:22:54 -0500

    Imported PyGame and created most basic initialization to create a game window

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import pygame
+from settings import Settings
+
+pygame.init()
+
+'''Init settings'''
+settings = Settings()
+
+'''Set the display'''
+screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
 def main():
     print("Hello from tractorbeam!")
 
 
 if __name__ == "__main__":
     main()
+
+pygame.quit()
---
+++ settings.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Settings:
+    screen_width: int = 1800
+    screen_height: int = 1200
