327c3edf833aff73635247852342e2f66fdae2fa
Lewis Miller <lewis.miller@gmail.com>
2026-07-19 21:42:40 -0500

    Fixed the scale factor

--- src/main.py
+++ src/main.py
@@ -20,15 +20,25 @@ def main():
     '''Set the display'''
     display_scale = get_max_displayscale(settings.game_width, settings.game_height)
     pygame.display.set_icon(game_icon)
-    screen = pygame.display.set_mode((settings.game_width, settings.game_height))
+    screen = pygame.display.set_mode((
+        settings.game_width * display_scale,
+        settings.game_height * display_scale))
     pygame.display.set_caption("TractorBeam")
+    game_surface = pygame.Surface((settings.game_width, settings.game_height))
 
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        
+    scaled = pygame.transform.scale(
+        game_surface,(
+            settings.game_width * display_scale,
+            settings.game_height * display_scale
+            )
+        )
+    screen.blit(scaled, (0,0))
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
